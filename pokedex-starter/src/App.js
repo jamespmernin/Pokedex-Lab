83f799d38380d9077cd6fb3d9b838b0369fe6b89
James Mernin <jamespmernin@gmail.com>
2020-09-30 17:09:16 -0400

    Finished HW

--- pokedex-starter/src/App.js
+++ pokedex-starter/src/App.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import axios from "axios";
+
+function capitalize(word) {
+  if (typeof (word) === 'string') {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+}
+
+function convertNumber(number) {
+  if (typeof (number) === 'number') {
+    return number / 10;
+  }
+}
 
 function App() {
+  const [pokemon, setPokemon] = useState({});
+  const [newSearch, setNewSearch] = useState();
+
+  useEffect(() => {
+    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${newSearch}`;
+    const makeApiCall = async () => {
+      const res = await axios.get(pokemonUrl);
+      setPokemon(res.data);
+    }
+    makeApiCall()
+  }, [newSearch])
+
   return (
     <>
       <header className="bg-dark d-flex justify-content-between px-5">
@@ -21,6 +46,17 @@ function App() {
                 Pokédex
               </span>
             </a>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              setNewSearch(pokemon);
+              setPokemon("");
+            }}>
+              <label htmlFor="pokemon"></label>
+              <input name="pokemon" type="search" placeholder="search for a Pokemon" onChange={(e) => {
+                setPokemon(e.target.value.toLowerCase())
+              }} />
+              <button type="submit">Submit</button>
+            </form>
           </div>
         </nav>
       </header>
@@ -29,6 +65,12 @@ function App() {
           id="results"
           className="d-flex justify-content-center flex-wrap col-10"
         ></section>
+        <div id="resultChart">
+          <h3>Name: {capitalize(pokemon.name)}</h3>
+          <h4>#{pokemon.id}</h4>
+          <h4>Height: {convertNumber(pokemon.height)} m</h4>
+          <h4>Weight: {convertNumber(pokemon.weight)} kg</h4>
+        </div>
       </main>
     </>
   );
